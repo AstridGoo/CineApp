62c63b3adbe7c6737236731eb50588390902e1dd
AstridGoo <74766080+AstridGoo@users.noreply.github.com>
2022-11-08 17:23:51 +0100

    Add Button
    Button to add new movie in movieList

--- app/src/main/java/com/example/cineapp/ListViewSample.java
+++ app/src/main/java/com/example/cineapp/ListViewSample.java
@@ -2,15 +2,18 @@ package com.example.cineapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class ListViewSample extends AppCompatActivity {
-    String movieList[] = {"Indiana Jones", "Harry Potter", "James Bond", "Toy Story", "L'Avare", "OSS 117"};
-    int movieImage[] = {R.drawable.icon_cinema, R.drawable.icon_tv, R.drawable.icon_cinema,
+    private Button button;
+    String[] movieList = {"Indiana Jones", "Harry Potter", "James Bond", "Toy Story", "L'Avare", "OSS 117"};
+    int[] movieImage = {R.drawable.icon_cinema, R.drawable.icon_tv, R.drawable.icon_cinema,
             R.drawable.icon_tv, R.drawable.icon_theatre, R.drawable.icon_tv};
 
     ListView listView;
@@ -28,5 +31,18 @@ public class ListViewSample extends AppCompatActivity {
                 Log.i("CUSTOM_LIST_VIEW", "Item is clicked @ Position :: " + position);
             }
         });
+
+        button = (Button) findViewById(R.id.buttonPlus);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAddMovie();
+            }
+        });
+    }
+
+    public void openAddMovie(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
