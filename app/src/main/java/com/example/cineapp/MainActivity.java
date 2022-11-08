ca29e0b8e05a89364578406937f5e32c42f62e45
AstridGoo <74766080+AstridGoo@users.noreply.github.com>
2022-11-08 11:25:22 +0100

    Maj
    Centralisation du bouton enregistre
    Ajout fichier ListView

--- app/src/main/java/com/example/cineapp/MainActivity.java
+++ app/src/main/java/com/example/cineapp/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.movie_list);
         initDatePicker();
         dateButton = findViewById(R.id.filmDate);
         dateButton.setText(getTodaysDate());
